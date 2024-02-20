ac79aaf7110c9cc1d761bbca46e7d1d25c244973
Elodie0207 <elodieguldas@gmail.com>
2024-02-20 20:51:17 +0100

    Inital Commit

---
+++ Assets/Scripts/Biche.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Biche : MonoBehaviour
+{
+    private double health = 100;
+    private double hunger = 100;
+    private double thirsty = 100;
+    private string genre = "";
+    void Start()
+    {
+        Console.WriteLine("Hello");
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
